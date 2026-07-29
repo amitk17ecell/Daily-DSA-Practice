15f84ef910e87e39c169f876204e090a0e051742
amitk17ecell <amit171105@gmail.com>
2026-07-29 23:57:25 +0530

    leetcode_33.cpp updated

---
+++ ARRAY/BinarySearch/leetcode_33.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std ; 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] == target) return mid;
+            
+            // Left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
+
+int main ()
+{ vector<int> arr = {4,5,6,8,10,1,2,3} ; 
+  int target = 3 ; 
+  Solution obj ; 
+  int result = obj.search(arr , target) ; 
+cout << result ; 
+}
