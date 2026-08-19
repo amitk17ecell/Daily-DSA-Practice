e6f23b3d2b98d167221c2189c129743c97760951
amitk17ecell <amit171105@gmail.com>
2026-08-19 21:30:06 +0530

    adding code chef solution 19/08/2026

---
+++ Contest/CodeChef/ioigold.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N,G ;
+	cin >> N >> G ;
+	if(N>=G)
+	  cout << "Yes" ; 
+	else 
+	  cout << "No" ; 
+	return 0 ; 
+
+}
