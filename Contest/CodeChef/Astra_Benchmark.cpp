d3d53927b708a17415efe4a80f160c9ef6df0ec3
amitk17ecell <amit171105@gmail.com>
2026-09-07 19:16:21 +0530

    adding code chef contet soln . Q1 07/09/2026

---
+++ Contest/CodeChef/Astra_Benchmark.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int A, B , X ; 
+    cin >> A >> B >> X ; 
+    if(A+B>=X)
+      cout << "YES" ; 
+    else 
+      cout << "NO" ; 
+    return 0 ; 
+}
