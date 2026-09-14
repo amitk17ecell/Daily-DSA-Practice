ce0d3d2ac8be3a26ec8a7e1cc894e62c9feeba26
amitk17ecell <amit171105@gmail.com>
2026-09-14 21:06:30 +0530

    adding codechef contest solution 14/09/2026

---
+++ Contest/CodeChef/pen_book.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int A ,B ; 
+    cin >> A >> B ; 
+    if(A<B)
+     cout << A ; 
+    else 
+     cout << B ; 
+    return 0  ; 
+}
